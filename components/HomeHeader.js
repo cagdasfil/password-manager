35284a310a6a61fef3110e482c22f42d2e21dbde
CAGDAS FIL <e2093839@ceng.metu.edu.tr>
2020-07-31 16:26:25 +0300

    search button added

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
 
 export default class HomeHeader extends React.Component{
     render(){
         return (
             <View style={styles.container}>
                 <Text style={styles.text}>Şifrelerim</Text>
+                <TouchableOpacity style={styles.search}>
+                    <Ionicons name="md-search" size={20} color="white"/>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -14,6 +17,7 @@ export default class HomeHeader extends React.Component{
 
 const styles = StyleSheet.create({
     container:{
+        position:"relative",
         backgroundColor:"#23395B",
         height:80,
         paddingBottom:10,
@@ -23,5 +27,19 @@ const styles = StyleSheet.create({
     text:{
         color:"#FFFFFF",
         fontSize:20
+    },
+    search:{
+        position:"absolute",
+        bottom:0,
+        right:0,
+        width:26,
+        height:26,
+        margin:10,
+        marginRight:13,
+        alignItems:"center",
+        justifyContent:"center",
+        borderWidth:1,
+        borderRadius:15,
+        borderColor:"white"
     }
 });
